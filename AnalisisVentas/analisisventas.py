68aa43b8fa4a5c26b7490319ce02bd36e9509e16
PaulLopez12 <paul.lopez.b@uni.pe>
2025-09-16 19:48:35 -0500

    analisisventas_V1.0

--- AnalisisVentas/analisisventas.py
+++ AnalisisVentas/analisisventas.py
@@ -1,31 +1,49 @@
 #1. Cargar datos del archivo csv
-import csv
-with open('ventas.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+from itertools import groupby
+from tokenize import group
+import pandas as pd
+df = pd.read_csv('ventas.csv')
+
 #2. Calcular el total de ventas por mes
-ventas_por_mes = {}
-for row in reader:
-    if row[0] not in ventas_por_mes:
-        ventas_por_mes[row[0]] = 0
-    ventas_por_mes[row[0]] += float(row[1])
-print(f"Las ventas por mes son: {ventas_por_mes}")
+df['mes'] = df['fecha'].dt.to_period('M')
+ventas_por_mes = df.groupby('mes').apply(lambda d: (d['cantidad'] * d['precio']).sum())
+ventas_por_mes = ventas_por_mes.sort_index()
+print("Ventas por mes:")
+print(ventas_por_mes)
 
 
 #3. Determinarr el producto mas vendido y con mayor ingresos
-producto_mas_vendido = max(reader, key=lambda x: x[1])
-print(f"El producto mas vendido es: {producto_mas_vendido}")
-producto_mas_ingresos = max(reader, key=lambda x: x[2])
-print(f"El producto con mayor ingresos es: {producto_mas_ingresos}")
+df['ingreso'] = df['cantidad'] * df['precio']
+ventas_prod = df.groupby('producto').agg({
+    'cantidad' : 'sum',
+    'ingreso' : sum
+})
+mas_vendido = ventas_prod['cantidad'].idxmax()
+mayor_ingreso = ventas_prod['ingreso'].idxmax()
+print(f"El producto con mayor ventas es: {mas_vendido} (total {ventas_prod.loc[mas_vendido, 'cantidad']})")
+print(f"El producto con mayores ingresos es: {mayor_ingreso} (total {ventas_prod.loc[mayor_ingreso, 'ingreso']:.2f} €)")
 
 #4. Graficar ventas por mes
 import matplotlib.pyplot as plt
-plt.bar(ventas_por_mes.keys(), ventas_por_mes.values())
+ventas_por_mes.index = ventas_por_mes.index.astype(str)
+plt.figure(figsize=(6,4))
+ventas_por_mes.plot(kind='bar')
+plt.title("Ventas por mes")
+plt.xlabel("Mes")
+plt.ylabel("Ventas (€)")
+
+plt.tight_layout()
+plt.savefig("VentasPorMes.png")
 plt.show()
 
 #5.Graficar top 5 productos por ingresos
-top_5_productos = sorted(reader, key=lambda x: x[2], reverse=True)[:5]
-plt.bar(top_5_productos.keys(), top_5_productos.values())
-plt.show()
+top5 = ventas_prod.nlargest(5, 'ingreso')
 
+plt.figure(figsize=(6,4))
+plt.bar(top5.index, top5['ingreso'])
+plt.title("Top 5 productos por Ingresos")
+plt.ylabel("Ingresos (€)")
+plt.xlabel("Producto")
+plt.tight_layout()
+plt.savefig("top5_productos.png")
+plt.show()
